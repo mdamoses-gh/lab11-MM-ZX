1c493eb37ccfcc85cce5c35ddc4d95caf1a2d3c4
Ziyue <Ziyue_xu@outlook.com>
2025-11-16 21:33:07 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,32 @@ calculator.py
 
 One function per operation, in order.
 """
+# https://github.com/mdamoses-gh/lab11-MM-ZX.git
+# Partner 1: Moises Miranda Manzanedo
+# Partner 2: Ziyue Xu
+import math
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except ZeroDivisionError:
+        pass
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError:
+        pass
+
+def exp(a, b):
+    return a ** b
 
