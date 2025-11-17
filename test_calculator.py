5e86a7e28d87eddfef131f1cd70d02e67e555f26
Moises Miranda <mda.moses@gmail.com>
2025-11-16 22:33:56 -0500

    Merged Partner 1 and Partner 2 changes

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/mdamoses-gh/lab11-MM-ZX.git
+# Partner 1: Moises Miranda Manzanedo
+# Partner 2: Ziyue Xu
+
 import unittest
 from calculator import *
 
@@ -14,12 +18,16 @@ class TestCalculator(unittest.TestCase):
         assert sub(3, 2) == 1
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        assert mul(2, 3) == 6
+        assert mul(-4, 5) == -20
+        assert mul(0, 10) == 0
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        assert div(2, 10) == 5
+        assert div(4, 20) == 5
+        assert div(-2, 10) == -5
+    ###########################
 
     ######## Partner 2
     def test_divide_by_zero(self): # 1 assertion
@@ -40,23 +48,29 @@ class TestCalculator(unittest.TestCase):
         log.assertRaises(ValueError, 5, 0)
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
-    #     fill in code
+        with self.assertRaises(ValueError):
+            log(10, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        assert hypotenuse(3, 4) == 5
+        assert hypotenuse(5, 12) == 13
+        assert hypotenuse(0, 0) == 0
 
-    # def test_sqrt(self): # 3 assertions
+    def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #    square_root(NUM)
     #     # Test basic function
-    #     fill in code
+        assert square_root(9) == 3
+        assert square_root(0) == 0
     ##########################
 
+    with self.assertRaises(ValueError):
+        square_root(-1)
 # Do not touch this
 if __name__ == "__main__":
     unittest.main()
